fb032a5ed275306749947a61d84f484af21bf600
acerise <42525915+acerise85@users.noreply.github.com>
2018-12-02 20:47:30 -0700

    Loops complete for questions, buttons added but need more styling

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,68 +1,109 @@
 
+$(document).ready(function(){
+    
     var questions = [{
         question: "How old was Judy Garland when she filmed the The Wizard of Oz?",
-        choices: [12, 14, 16, 18],
-        correctAnswer: 3,
+        choices: ["12", "14", "16", "18"],
+        correctAnswer: "16",
     },
     {
         question: "Who played the Cowardly Lion?",
         choices: ["Jack Haley",  "Gene Kelly", "Bert Lahr","Humphry Bogart"],
-        correctAnswer: 3,
+        correctAnswer: "Bert Lahr",
     },
 
     {
         question: "When was the Wizard of Oz filmed?",
-        choices:[1936, 1945, 1928, 1939],
-        correctAnswer: 4,
+        choices:["1936", "1945", "1928", "1939"],
+        correctAnswer: "1939",
     },
     {
         question: "Who played the Tin Man?",
         choices: ["Fred Astaire", "Jack Haley", "Ray Bolger", "Mickey Rooney"],
-        correctAnswer: 2,
+        correctAnswer: "Jake Haley",
     },
     {
         question: "How many Munchkins are in the Wizard of OZ?",
-        choices: [300, 124, 170, 230],
-        correctAnswer: 2,
+        choices: ["300", "124", "170", "230"],
+        correctAnswer: "124",
     },
     {
         question: "Who played the Scarecrow?",
         choices: ["Bert Lahr", "Frank Morgan","Ray Bolger", "Clark Gable", ],
-        correctAnswer: 3,
+        correctAnswer: "Ray Bolger",
     },
     {
         question: "What is the signature chant Dorothy, The Scarecrow, and The Tin Man say when walking through the forest?",
         choices: ["Lions and tigers and chipmunks, oh my!", "Lions and tigers and giraffs, oh my!", "Bengals and ligers and rats, oh my!", "Lions and tigers and bears, oh my!"],
-        correctAnswer: 4,
+        correctAnswer:  "Lions and tigers and bears, oh my!",
     },
     {
         question: "Which wicked witch did Dorothy's house fall on?",
         choices: ["East", "North", "South", "West"],
-        correctAnswer: 1,
+        correctAnswer: "East",
     },
     {
         question: "What does the Wicked Witch of the West say after Dorothy throws the water on her?",
         choices: ["I'm drenched! I'm drenched! I'm drenched!", "I'm melting! I'm melting! I'm melting!", "Now I have to change!", "I'm drowning! I'm drowning! I'm drowning!"],
-        correctAnswer: 2,
+        correctAnswer: "I'm melting! I'm melting! I'm melting!",
     },
     {
-        question: ["How old is the munchkin that hands Dorothy the lollipop?"],
-        choices: [12, 18, 14, 15 ],
-        correctAnswer: 2,
+        question: "How old is the munchkin that hands Dorothy the lollipop?",
+        choices: ["12", "18", "14", "15" ],
+        correctAnswer: "18",
     },
     ];
+    var right = 0;
+    var wrong = 0;
 
+        //loop through the questions array and display one question at a time
+        var index = 0;
+        function nextQuestion(){
+            $('#currentQuestion').html(questions[index].question);
+            $('#currentChoices').empty();
+            for (var i = 0; i < questions[index].choices.length; i++){
 
+                var newChoice = $('<button type="button" class="btn btn-success"></button>');
 
-        //loop through the questions array and display them to the screen with their choices
-        for (var i = 0; i < questions.choices; i++){
+                newChoice.text(questions[index].choices[i]);
 
-            var answerButton = $('<button>');
-            answerButton.text(choices[i]);
-            answerButton.attr('data-id', i);
-            $('#answer_div').append('button');
+                $('#currentChoices').append(newChoice);
+
+            }
         }
+        nextQuestion();
 
+        $(document).on('click','.btn',function(){
+            var value = $(this).text();
+            console.log(value);
+            if(value === questions[index].correctAnswer){
+                right++;
+                alert("you are right");
+            }
+            else{
+                wrong++;
+                alert("You are wrong")
+            }
+            index++;
+            nextQuestion();
+        })
+        var time = 10;
+        $('#gameTimer').html("Time Left: " + time);
+        var gameTimer = setInterval(function(){
+            time--;
+            $('#gameTimer').html("Time Left: " + time);
+            console.log(time)
+            if(time === 0){
+                alert("Game Over!")
+                clearInterval(gameTimer);
+            }
+                
+        },1000)
+})       
+        
+        
+        
+        //loop through the choices array in each question and display the choices to the screen in a button
         //when an answer is chosen it needs to go to the coorect answers count or inncorrect answers count
 
 
